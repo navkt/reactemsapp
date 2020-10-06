87a956eb0f1e39fcbc1616d4f99ffef40cd0871b
navkt <naveenkthota@gmail.com>
2020-10-06 16:39:16 +0530

    Updates

---
+++ precache-manifest.8b79c72e8d600df22fa75265e201d463.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a5466ff8e93b1234a244d6ebdc3d37c",
+    "url": "/reactemsapp/index.html"
+  },
+  {
+    "revision": "1936ca5d1309cdeccf2e",
+    "url": "/reactemsapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "401e821fa841eefdb965",
+    "url": "/reactemsapp/static/js/2.def5e3ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactemsapp/static/js/2.def5e3ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1936ca5d1309cdeccf2e",
+    "url": "/reactemsapp/static/js/main.a4ece5a5.chunk.js"
+  },
+  {
+    "revision": "31c319fcd00da32f4ff7",
+    "url": "/reactemsapp/static/js/runtime-main.9f99ae5c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactemsapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
